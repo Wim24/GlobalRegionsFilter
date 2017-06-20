fd2c8f75ca026373a13fc45be84ba0058299de8a
Wim Chen <664791648@qq.com>
2017-06-21 00:29:57 +0800

    Add files

---
+++ GlobalRegionsFilterTests/GlobalRegionsFilterTests.m
@@ -0,0 +1,39 @@
+//
+//  GlobalRegionsFilterTests.m
+//  GlobalRegionsFilterTests
+//
+//  Created by chenwei on 2016/12/15.
+//  Copyright © 2016年 Wim. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GlobalRegionsFilterTests : XCTestCase
+
+@end
+
+@implementation GlobalRegionsFilterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
